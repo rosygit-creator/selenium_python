7489c6f2b170b0b155df2b2192f0369c0e3cc9a0
rosygit-creator <rosy.github@gmail.com>
2025-11-04 10:40:58 -0800

    alert

---
+++ switch_alert.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# Initialize the WebDriver (replace with your WebDriver's path if not in PATH)
+# Example for Chrome:
+driver = webdriver.Chrome() 
+
+# Example for Firefox (if GeckoDriver is in PATH):
+# driver = webdriver.Firefox()
+
+# Navigate to a website
+driver.get("https://formy-project.herokuapp.com/switch-window")
+
+original_handle=driver.current_window_handle
+# Interact with elements - open new window
+
+driver.find_element(By.ID, "alert-button").click()
+# print("new window")
+
+# switch to alert
+alert=driver.switch_to.alert
+# print(alert.text)
+
+assert alert.text=="This is a test alert!"        
+    
+# text_form=driver.find_element(By.CLASS_NAME, "//*[@class='display-3']")
+# print(text_form.text)
+
+# Close the browser
+driver.quit()
---
+++ switch_window.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# Initialize the WebDriver (replace with your WebDriver's path if not in PATH)
+# Example for Chrome:
+driver = webdriver.Chrome() 
+
+# Example for Firefox (if GeckoDriver is in PATH):
+# driver = webdriver.Firefox()
+
+# Navigate to a website
+driver.get("https://formy-project.herokuapp.com/switch-window")
+
+original_handle=driver.current_window_handle
+# Interact with elements - open new window
+
+driver.find_element(By.ID, "new-tab-button").click()
+# print("new window")
+
+# get all window handles
+all_windows=driver.window_handles
+
+for h in all_windows:
+    if h!=original_handle:
+        driver.switch_to.window(h)
+  
+assert driver.title=="Formy"        
+    
+# text_form=driver.find_element(By.CLASS_NAME, "//*[@class='display-3']")
+# print(text_form.text)
+
+# Close the browser
+driver.quit()

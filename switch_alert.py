0d897062794b608cc629f7a13c143f0138c990b1
rosygit-creator <rosy.github@gmail.com>
2025-11-04 11:22:04 -0800

    alert

--- switch_alert.py
+++ switch_alert.py
@@ -15,17 +15,18 @@ driver.get("https://formy-project.herokuapp.com/switch-window")
 original_handle=driver.current_window_handle
 # Interact with elements - open new window
 
-driver.find_element(By.ID, "alert-button").click()
-# print("new window")
+driver.find_element(By.ID, "alert-button").click
 
 # switch to alert
 alert=driver.switch_to.alert
 # print(alert.text)
 
 assert alert.text=="This is a test alert!"        
-    
-# text_form=driver.find_element(By.CLASS_NAME, "//*[@class='display-3']")
-# print(text_form.text)
+alert.accept
+
+# verify alert is accepted
+text_form=driver.find_element(By.ID, "new-tab-button")
+print(text_form.text)
 
 # Close the browser
 driver.quit()
